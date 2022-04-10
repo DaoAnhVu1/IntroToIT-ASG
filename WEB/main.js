a7fde7a9d03b06a8c56d713457b7cebb70526d75
DaoAnhVu <s3926187@rmit.edu.vn>
2022-04-10 17:00:06 +0700

    make the header responsive

--- WEB/main.js
+++ WEB/main.js
@@ -1,4 +1,9 @@
 const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('.nav .list-link');
+const menuToggleIcon = document.getElementById('menu-toggle-icon');
+
+console.log(nav)
+// Header Scroll
 window.addEventListener('scroll', () => {
     if(window.scrollY >= 500) {
         nav.classList.add('nav-black');
@@ -6,3 +11,22 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('nav-black');
     }
 })
+
+// toggle
+const toggleMenu = () => {
+    nav.classList.toggle('active');
+    menuToggleIcon.classList.toggle('active');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu) 
+
+
+// Nav-Link modification
+
+navLinks.forEach(link => link.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menuToggleIcon.classList.toggle('active');
+    let current = document.getElementsByClassName('current');
+    current[0].className = current[0].className.replace(' current', '');
+    link.className += ' current';
+}))
